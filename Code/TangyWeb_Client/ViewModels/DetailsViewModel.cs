c0246234d31ca477b4b8908f7395b95b6fee2d95
Ritesh16 <ritesh.sharma.cse@gmail.com>
2022-12-27 07:59:44 -0500

    Adding details page and view model

---
+++ Code/TangyWeb_Client/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tangy_Models.Dtos;
+
+namespace TangyWeb_Client.ViewModels
+{
+    public class DetailsViewModel
+    {
+        public DetailsViewModel()
+        {
+            ProductPrice = new();
+            Count = 1;
+        }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value greater than 0")]
+        public int Count { get; set; }
+        [Required]
+        public int SelectedProductPriceId { get; set; }
+        public ProductPriceDto ProductPrice { get; set; }
+    }
+}
